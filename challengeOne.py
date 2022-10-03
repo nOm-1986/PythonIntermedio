6c0030911cb43d4def48884ec983f7e8ac89aa42
José Fabián Beltrán Meza <joshefbeltran@gmail.com>
2022-10-03 12:20:48 -0500

    Reto finalizado

--- challengeOne.py
+++ challengeOne.py
@@ -2,5 +2,8 @@ def run():
     clist = [a**2 for a in range(1,101)]
     print(clist, "Mensaje")
 
+    clist2 = [a**2 for a in range(1,101) if a % 3 != 0]
+    print(clist2, "Dif")
+
 if __name__ == '__main__':
     run()
