602e5a45fcae9cae27c190f5117f153c5e9d4b2e
José Fabián Beltrán Meza <joshefbeltran@gmail.com>
2022-10-03 11:06:51 -0500

    Iniciando curso Python intermedio con entornos virtuales

---
+++ challengeOne.py
@@ -0,0 +1,6 @@
+def run():
+    clist = [a**2 for a in range(1,101)]
+    print(clist)
+
+if __name__ == '__main__':
+    run()
---
+++ lists_and_dicts.py
@@ -0,0 +1,36 @@
+def run():
+    my_list = [1, 'Hello', True, 4.5, 'Fabian']
+    my_dict = {"firstname" : "Fabián", "lastname" : "Beltran"}
+
+    super_list = [
+        {"firstname" : "Fabián", "lastname" : "Beltran"},
+        {"firstname" : "Maria", "lastname" : "Beltran"},
+        {"firstname" : "Karen", "lastname" : "Rivera"},
+        {"firstname" : "Adrian", "lastname" : "Beltran"}
+    ]
+
+    super_dict = {
+        "natural_nums" : [1,2,3,4,5],
+        "integer_nums" : [-1,-2,0,1,2],
+        "floating_nums": [1.1, 4.5, 6.43, 5.86]
+    }
+
+    for key,value in super_dict.items():
+        print(key, " - ", value)
+
+
+    print("\n===========================================\n")
+
+    for a in super_list:
+        for k,v in a.items():
+            print(k, " - ", v)
+
+
+    print("\n===========================================\n")
+
+    for a in super_list:
+        print(f'{a["firstname"]} {a["lastname"]}')
+
+
+if __name__ == '__main__':
+    run()
