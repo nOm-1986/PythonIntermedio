fc989d5412b75f6b1d98f25c4add0785ee4f42b4
Fabian Beltran <joshefbeltran@gmail.com>
2022-10-04 12:08:38 -0500

    Definiciones de lambda y high order functions

--- lambda_functions.py
+++ lambda_functions.py
@@ -2,6 +2,8 @@
     Son funciones anónimas, pueden ser utilizadas como argumentos para otras funciones
     o almacenadas en simples variables.
     Estas funciones pueden ser utilizadas como argumento dentro de otras.
+    Pueden tener varios argumentos, pero solo una línea de código.
+    lambda argumentos: expresión
 """
 
 f_x3 = lambda x: x * 3
@@ -9,3 +11,7 @@ print(f_x3(10))
 print(f_x3(14))
 print(f_x3(16))
 print(f_x3(20))
+
+palindrome = lambda string: string == string[::-1]
+print(palindrome('ojo'))
+print(palindrome('no es'))
