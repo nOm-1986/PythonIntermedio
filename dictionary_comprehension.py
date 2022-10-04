873e057d26866ac60c29f605b48fc47002183526
Fabian Beltran <joshefbeltran@gmail.com>
2022-10-04 11:41:59 -0500

    Iniciando los ejemplos con lambdas

---
+++ dictionary_comprehension.py
@@ -0,0 +1,6 @@
+def run():
+    one = {x:x**3 for x in range(1,101)}
+    print(one)
+
+if __name__ == '__main__':
+    run()
---
+++ lambda_functions.py
@@ -0,0 +1,11 @@
+""" Qué significa lambda?
+    Son funciones anónimas, pueden ser utilizadas como argumentos para otras funciones
+    o almacenadas en simples variables.
+    Estas funciones pueden ser utilizadas como argumento dentro de otras.
+"""
+
+f_x3 = lambda x: x * 3
+print(f_x3(10))
+print(f_x3(14))
+print(f_x3(16))
+print(f_x3(20))
