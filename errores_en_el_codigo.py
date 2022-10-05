ca397429a7d6daab29c1bdb36ef6a31b91f7ba38
Fabian Beltran <joshefbeltran@gmail.com>
2022-10-05 16:44:01 -0500

    Tipos de errores y debbugin

---
+++ errores_en_el_codigo.py
@@ -0,0 +1,30 @@
+""" Los errores en el código:
+    * - Cuando python nos avisa que nos equivocamos:
+            Devuelve un traceback
+            -SyntaxError: Cuando escribimos mal el nombre de una función. - Detiene el programa, no se ejecuta.
+            -Exception: Suceden en un puton del programa que quiebra toda la lógica. Lo hace en una línea específica.
+                -KeyboardInterrupt: pulsamos ctrl + c interrumpiendo el proceso en consola.
+                -KeyError: Cuando intentamos acceder a una llave en un diccionario y esta no existe.
+                -IndexError: Cuando trabajamos con listas y un indice en esa lista no existe.
+                -FileNotFoundError: Intentamos abrir un archivo que no existe
+                -ZeroDivisionError: Intentamos dividir un número entre cero
+                -ImportError: Cuando intentamos importar algun módulo y hay algún error en el módulo y algo falla.
+        -Consultar los tipos de excepciones que maneja python
+            
+
+    * - Cuando python no nos avisa:
+            Haciendo debbugin
+"""
+def divisors(num):
+    divisors = [ x for x in range(1, num + 1) if num % x == 0]
+    return divisors
+
+
+def run():
+    num = int(input('Favor ingrese un número: '))
+    print(divisors(num))
+    print('Terminó mi programa')
+
+
+if __name__ == '__main__':
+    run()
