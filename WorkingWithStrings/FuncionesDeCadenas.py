d1fe6b8925f0caa1ce772c56d8dea4ebac3ecc9a
Fabian Beltran <joshefbeltran@gmail.com>
2022-10-18 17:00:15 -0500

    Starting with String functions using the book

--- WorkingWithStrings/FuncionesDeCadenas.py
+++ WorkingWithStrings/FuncionesDeCadenas.py
@@ -6,5 +6,21 @@ def stringsToChars():
     print(str(a), str(b), str(c), str(d), str(e))
 
 
+def dateToChar():
+    something = str(datetime.datetime(2022, 5,8, 5, 35))
+    print(type(something), something)
+
+def strings_to_numbers():
+    '''Python lets us transform characters in numbers corresponding to Unicode table using the ord function. 
+    '''
+    my_var = ''.join([chr(x+1) for x in range(200)])
+    print(my_var)
+
+    my_var2 = ' '.join([chr(x+9812) for x in range(12)])
+    print(my_var2)
+
+
 if __name__ == '__main__':
-    stringsToChars()
+    #stringsToChars()
+    #dateToChar()
+    strings_to_numbers()
