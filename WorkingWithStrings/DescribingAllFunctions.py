bf3bf2740e0f877e102fe54a0dc842de2c350c5d
Fabian Beltran <joshefbeltran@gmail.com>
2022-10-25 15:29:58 -0500

    Starting the strings functions summary in python until 3.10 version

---
+++ WorkingWithStrings/DescribingAllFunctions.py
@@ -0,0 +1,15 @@
+"""
+    Describiendo todas las funciones presentes en python en la versión 3.10
+
+    1.	capitaliza      Cambia todas las letras a minúsculas menos la primera.
+    2.	casefold        Utilizada para compara cadenas sin importar el tamaño de los caracteres.
+    3.  center          Formaseta una cadena alineandola al centro
+    4.  count           Cuenta las ocurrencias de una cadena
+    5.  encode          Codifica una cadena según la codificación deseada.
+    6.  endswith        Comprueba si la cadena termina con una cadena específica.
+    7.  expandtabs      Convierte los tabuladores en espacios.
+    8.  find            Devuelve el índice de la cadena buscada o -1 si no se encuentra.
+    9.  format          Formatea una cadena de forma avanzada.
+    10. format_map      Igual que format, pero sin hacer una copia de los parámetros.
+    11. index           Devuelve el índice 
+"""
