a755004fcdfa03a197ee3cd16d1f903085bf4105
José Fabián Beltrán Meza <joshefbeltran@gmail.com>
2025-01-17 19:20:11 -0500

    Working with MAP

---
+++ InterNico/14_map.py
@@ -0,0 +1,12 @@
+numbers = [x for x in range(1, 20)]
+numbers_v2 = []
+
+numbers_v2 = list(map(lambda x : x * 2, numbers))
+print(numbers_v2)
+print('++' * 30)
+# Iterando 2 listas de distinta longitud
+numbers_1 = [x for x in range(1, 5)]
+numbers_2 = [x for x in range(1, 4)]
+
+result = list(map(lambda x, y: x + y, numbers_1, numbers_2))
+print(result)
