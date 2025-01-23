e5398b5979c323c9a00cf4f75916bfbb58c86aa6
José Fabián Beltrán Meza <joshefbeltran@gmail.com>
2025-01-22 20:48:19 -0500

    Working with files again

---
+++ InterNico/22_challenger_error.py
@@ -0,0 +1,12 @@
+def my_divide(a, b):
+   try:
+      result = a / b
+   except ZeroDivisionError:
+      result = 'No se puede dividir por 0'
+   return result
+    
+response = my_divide(10, 2)
+print(response) # 5
+
+response = my_divide(10, 0)
+print(response) # No se puede dividir por 0
---
+++ InterNico/23_read_files.py
@@ -0,0 +1,28 @@
+file = open("../archivos/names.txt")
+print(file.readline())
+
+def read():
+    #number = list()
+    with open("../archivos/numbers.txt", "r", encoding="utf-8") as f:
+        number = [ int( x ) for x in f]
+        # for line in f:
+        #     number.append(int(line))
+    print(number)
+
+
+def write():
+    names = ['Fabian', 'Adrian', 'Leidy', 'María José', 'Karen','Fabiola', 'Alberto']
+    with open('./archivos/names.txt', "w", encoding='utf-8') as f:
+    # with open('./archivos/names.txt', "a", encoding='utf-8') as f: -> usando a de append
+        for name in names:
+            f.write(name)
+            f.write("\n")
+
+
+def run():
+    read()
+    write() 
+
+
+if __name__ == '__main__':
+    run()
