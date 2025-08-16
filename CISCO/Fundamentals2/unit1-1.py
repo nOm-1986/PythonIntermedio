a05fe00b3160e841cc3673d4acde2c81ae0b6659
José Fabián Beltrán Meza <joshefbeltran@gmail.com>
2025-08-15 20:47:16 -0500

    Starting fundamentals 2. Modules and how to import it

---
+++ CISCO/Fundamentals2/unit1-1.py
@@ -0,0 +1,34 @@
+#import math, os, system
+from math import sin, pi
+# from math import * ---- Forma agresiva pero puede chocar con otros nombres de funciones
+# import module as conalias
+# from module import n as n_m, m as b_m, ....
+
+
+# def sin(x):
+#   if 2 * x == pi:
+#     return 0.99999999
+#   else:
+#     return None
+
+# pi = 3.14
+#print(math.sin(math.pi/2))
+#print(sin(pi / 2))
+
+print(sin(pi/2))
+
+pi = 3.14
+
+def sin(x):
+    if 2 * x == pi:
+        return 0.99999999
+    else:
+        return None
+
+print(sin(pi / 2))
+
+
+# ===================== CHALLENGEEE ==================== #
+# 1 -- import mint ---> mint.make_money()
+# 2 -- from mint import make_money ----> make_money()
+# 3 -- from mint import make_money as m_m
---
+++ CISCO/Fundamentals2/unit1-2.py
@@ -0,0 +1,14 @@
+dir(math)
+from math import floor, ceil, trunc
+# for name in dir(math):
+  # print(name, end="\t")
+
+x = 1.4
+y = 2.6
+
+print(floor(x), floor(y))
+print(floor(-x), floor(-y))
+print(ceil(x), ceil(y))
+print(ceil(-x), ceil(-y))
+print(trunc(x), trunc(y))
+print(trunc(-x), trunc(-y))
