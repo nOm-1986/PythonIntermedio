e58644cd4f87f5c9ab77c385ac61f86b0cedce87
José Fabián Beltrán Meza <joshefbeltran@gmail.com>
2025-08-26 22:55:18 -0500

    Strings and its methods

---
+++ CISCO/Fundamentals2/Unit2/Cadenas1/unit2.1-strings.py
@@ -0,0 +1,17 @@
+"""
+Las cadenas en python son secuencias inmutables
+"""
+
+string_multyline = '''
+I can write multilines in python using three single quotes, or doble quotes
+at the beging and another three in the end.
+'''
+
+string_two_quotes = """
+Remember that specials characters like a \ n count
+"""
+
+# String functions
+# ord() - Show the ASCII 
+
+
---
+++ CISCO/Fundamentals2/Unit2/unit2.1.py
@@ -0,0 +1,18 @@
+"""
+ASCII (American Standar Code for Information Interchange)
+
+I18N -- Internationalization
+
+Unicode -- Unicode asigna caracteres únicos (letras, guiones, ideogramas, etc.) a más de un millón de puntos de código.
+
+UTF-8 -- El nombre se deriva de Unicode Transformation Format (Formato de Transformación Unicode).
+
+Python 3 es totalmente compatible con Unicode y UTF-8:
+
+Puedes usar caracteres codificados Unicode/UTF-8 para nombrar variables y otras entidades.
+Puedes usarlos durante todas las entradas y salidas.
+Esto significa que Python3 está completamente Internacionalizado.
+
+BOM (byte order mark - marca de orden de bytes)
+BOM (Byte Order Mark), una Marca de Orden de Bytes es una combinación especial de bits que anuncia la codificación utilizada por el contenido de un archivo (por ejemplo, UCS-4 o UTF-8).
+"""
